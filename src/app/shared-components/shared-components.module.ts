e10f8b6733fc2c1fa77f71f76934c3305e13b5ce
Christian Kubel Højmose <hjmose@gmail.com>
2022-11-30 21:30:57 +0100

    feat: main layout created

--- src/app/shared-components/shared-components.module.ts
+++ src/app/shared-components/shared-components.module.ts
@@ -3,16 +3,18 @@ import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
-import { MatLegacyButtonModule as MatButtonModule } from '@angular/material/legacy-button';
+import { MatButtonModule } from '@angular/material/button';
 import { SplitterbarComponent } from './splitterbar/splitterbar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [NavbarComponent, SplitterbarComponent],
+  declarations: [NavbarComponent, SplitterbarComponent, FooterComponent],
   exports: [
     NavbarComponent,
     MatIconModule,
     MatButtonModule,
     SplitterbarComponent,
+    FooterComponent
   ],
   imports: [CommonModule, MatToolbarModule, MatIconModule, MatButtonModule],
 })
