8f1ff926136572b6854b61209ec8a2b6f2d9afd2
Christian Kubel Højmose <christian@hjmose.dk>
2023-08-09 21:39:44 +0200

    refactor(all)!: did some general clean up
    removed all non used comments, and removed all tests -
    as the app will beend-to-end tested via cypress
    
    BREAKING CHANGE: all current test files removed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,45 +7,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Rainbow Baby Dragon Shop';
-
-  image1 =
-    'https://images.unsplash.com/photo-1567653418876-5bb0e566e1c2?ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80';
-  image2 =
-    'https://images.unsplash.com/photo-1559181567-c3190ca9959b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2000&q=80';
-  image3 =
-    'https://images.unsplash.com/photo-1557800634-7bf3c7305596?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2001&q=80';
-  image4 =
-    'https://images.unsplash.com/photo-1551410224-699683e15636?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2000&q=80';
-
-  carouselItems = [this.image1, this.image2, this.image3, this.image4];
-
-  allItems = [
-    {
-      title: 'Mobile',
-      image: 'assets/images/mobile-img.png',
-      description: 'This is the description of the product',
-      brand: 'Samsung',
-      percentage: '70 %',
-      price: 999,
-      onSale: true,
-    },
-    {
-      title: 'Computer',
-      image: 'assets/images/computer-img.png',
-      description: 'This is the description of the product',
-      brand: 'LG',
-      percentage: '70 %',
-      price: 9999,
-      onSale: false,
-    },
-    {
-      title: 'Camera',
-      image: 'assets/images/camera-img.png',
-      description: 'This is the description of the product',
-      brand: 'LG',
-      percentage: '70 %',
-      price: 500,
-      onSale: true,
-    },
-  ];
 }
