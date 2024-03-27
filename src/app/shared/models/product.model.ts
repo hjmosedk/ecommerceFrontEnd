ba2e129da7f2a589d04cee7b60859e394ac2f36b
Christian Kubel Højmose <christian@hjmose.dk>
2024-03-27 20:54:12 +0100

    refactor(models): adjusted some models in the product module

--- src/app/shared/models/product.model.ts
+++ src/app/shared/models/product.model.ts
@@ -23,31 +23,3 @@ export interface ProductModel {
 }
 
 export type NewProductModel = Omit<ProductModel, 'id'>;
-
-export class PersonalInformationModel {
-  constructor(
-    public firstName: string = '',
-    public lastName: string = '',
-    public email: string = '',
-    public phone: number = 0,
-    public middleName?: string
-  ) {}
-}
-
-export class AddressModel {
-  constructor(
-    public address: string = '',
-    public address2nd?: string,
-    public city: string = '',
-    public country: string = '',
-    public zipCode: string = ''
-  ) {}
-}
-
-export class CustomerModel {
-  constructor(
-    public personalInformation: PersonalInformationModel,
-    public shippingAddress: AddressModel,
-    public billingAddress: AddressModel
-  ) {}
-}
